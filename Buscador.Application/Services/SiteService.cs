ebef599bd71124ef44342940b165598433c0aff8
mellofe <felipe.mello@ufv.br>
2024-02-22 21:20:54 -0300

    Implementação inicial do método de busca

--- Buscador.Application/Services/SiteService.cs
+++ Buscador.Application/Services/SiteService.cs
@@ -1,14 +1,33 @@
-﻿using Buscador.Application.Services.Interfaces;
+﻿using AutoMapper;
+using Buscador.Application.Services.Interfaces;
 using Buscador.Domain.Dtos.Requests;
 using Buscador.Domain.Dtos.Responses;
+using Microsoft.Extensions.Configuration;
+using Nest;
 
 namespace Buscador.Application.Services
 {
     public class SiteService : ISiteService
     {
+        private IElasticClient ElasticClient { get; }
+        private IMapper Mapper { get; }
+        private IConfiguration Configuration { get; }
+
+        public SiteService(IElasticClient elasticClient, IMapper mapper, IConfiguration configuration)
+        {
+            ElasticClient = elasticClient;
+            Mapper = mapper;
+            Configuration = configuration;
+        }
+
         public Task<List<SiteResponseDto>> SearchPagenedSites(PagenedSiteSearchDto pagenedSiteSearchDto)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(pagenedSiteSearchDto.Title) || pagenedSiteSearchDto.PageIndex < 0 || pagenedSiteSearchDto.TotalItems < 0)
+            {
+                return Task.FromResult<List<SiteResponseDto>>(null);
+            }
+
+            return Task.FromResult<List<SiteResponseDto>>(null);
         }
     }
 }
--- Buscador/Program.cs
+++ Buscador/Program.cs
@@ -1,3 +1,6 @@
+using Buscador.Application.Services;
+using Buscador.Application.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<ISiteService, SiteService>();
 
 var app = builder.Build();
 
