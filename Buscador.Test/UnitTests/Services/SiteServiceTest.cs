4b95cea5ac85b69fdefda375ddddc05368a37b32
mellofe <felipe.mello@ufv.br>
2024-02-23 21:07:44 -0300

    Estrutura inicial dos testes automáticos

---
+++ Buscador.Test/UnitTests/Services/SiteServiceTest.cs
@@ -0,0 +1,35 @@
+﻿using Buscador.Application.Services;
+using Buscador.Application.Services.Interfaces;
+using Buscador.Domain.Dtos.Requests;
+using Buscador.Test.UnitTests.Services.Stubs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Nest;
+
+namespace Buscador.Test.UnitTests.Services
+{
+	public class SiteServiceTest
+	{
+		private ISiteService SiteService { get; set; }
+
+		public SiteServiceTest()
+		{
+			IServiceCollection services = new ServiceCollection();
+			services.AddScoped<ISiteService, SiteService>();
+			services.AddScoped<IElasticClient, ElasticClientStub>();
+			services.AddAutoMapper(typeof(SiteService).Assembly);
+			services.AddScoped<IConfiguration, ConfigurationStub>();
+			ServiceProvider serviceProvider = services.BuildServiceProvider();
+
+			SiteService = serviceProvider.GetRequiredService<ISiteService>();
+		}
+
+		[Fact]
+		public void SearchPagenedSites_Sucess()
+		{
+			var request = new PagenedSiteSearchDto();
+			var result = SiteService.SearchPagenedSites(request);
+			var t = 0;
+		}
+	}
+}
